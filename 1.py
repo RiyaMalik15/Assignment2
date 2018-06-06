de7be32577a9c2d42db50ca6e988ae7f9fa41dfe
RiyaMalik15 <riyamalik608@gmail.com>
2018-06-06 22:15:08 +0530

    first change

---
+++ 1.py
@@ -0,0 +1,32 @@
+print("Questin 1")
+print("My name is Riya Malik")
+print("*"*10)
+print('\n')
+print("Question 2")
+x="Acad"
+y="View"
+print(x+y)
+print("*"*10)
+print('\n')
+print("Queston 3")
+x=input("Enter name:")
+y=input("Enter email:")
+z=input("Enter Mobile_number:")
+print(x,y,z,sep='\n')
+print("*"*10)
+print('\n')
+print("Question 4")
+print('''"Let's get started"''')
+print("*"*10)
+print('\n')
+print("Question 5")
+s='''"Acadview"'''
+course='''"Python"'''
+Fees=5000
+print("s=%s\ncourse=%s\nFees=%s"%(s,course,Fees))
+print("*"*10)
+print('\n')
+print("Question 6")
+name='''"Tony Stark"'''
+salary=1000000000
+print('%s''%d'%(name,salary))
